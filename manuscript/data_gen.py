ef5b94f624850995320af2d9c0a2c4df68c92f7c
Marcello Coletti <coletti.marcello@gmail.com>
2023-02-15 11:31:32 -0500

    ETL finished

--- manuscript/data_gen.py
+++ manuscript/data_gen.py
@@ -6,20 +6,17 @@ def unify_data():
     df = pd.DataFrame()
 
     for i in os.listdir('data_raw/'):
-        if i != '.gitkeep':
-            df1 = pd.read_sas(f'data_raw/{i}', format='sas7bdat', encoding='utf-8')
-            try:
-                df1.columns = [i.lower() for i in ['year', 'Icode', 'Importer', 'Ecode', 'Exporter', 'sitc4', 'Value']]
-                df1 = df1[[i.lower() for i in ['year', 'Icode', 'Importer', 'Ecode', 'Exporter', 'sitc4', 'Value']]]
-                df1 = df1.groupby([i.lower() for i in ['year', 'Icode', 'Importer', 'Ecode', 'Exporter', 'sitc4', 'Value']]).sum().reset_index()
-                df1.columns = ['year', 'icode', 'importer', 'ecode', 'exporter', 'product', 'share']
-                df = pd.concat([df,df1])
-            except:
-                print('Error with file '+i)
-                print(df1)
+        df1 = pd.read_sas(f'data_raw/{i}', format='sas7bdat', encoding='utf-8')
+        print(f'Opening file {i}')
+        new_cols = [i.lower() for i in df1.columns]
+        df1.columns = new_cols
+        df1 = df1[['year', 'icode', 'importer', 'ecode', 'exporter', 'sitc4', 'quantity']]
+        df1 = df1.groupby(['year', 'icode', 'importer', 'ecode', 'exporter', 'sitc4']).sum().reset_index()
+        df1.columns = ['year', 'icode', 'importer', 'ecode', 'exporter', 'product', 'share']
+        df = pd.concat([df, df1])
 
     for c in ['year', 'icode', 'importer', 'ecode', 'exporter', 'product']:
-       df[c] = df[c].astype(str)
+        df[c] = df[c].astype(str)
         
     df['share'] = df['share'].astype(float)
     df.to_csv('data_cleaned/data.csv', index=False)
